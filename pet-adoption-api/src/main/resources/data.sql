910d301c5fcf1da33caa8220e42532bd575be457
ludmyla <ludy@ludmyla.com>
2025-09-25 15:22:14 -0300

    Mais uma atividade concluída do módulo 9

---
+++ pet-adoption-api/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Cachorrinho da EBAC', 5, 'SRD', current_date, 'Bem pulguento', 'Cleber', 'Pequeno');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES ('Outro cachorrinho da EBAC', 10, 'Rottweiler', current_date -10, 'Bem velhinho', 'Renato', 'Grande');
+
+INSERT INTO animal (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte, data_adocao)
+VALUES ('Totó', 2, 'Pinscher', current_date, '50% tremedeira, 50% ódio', 'Giovanna', 'Pequeno', current_date);
